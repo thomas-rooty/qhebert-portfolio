b24be33d2f1255bbc087e5ce1bc5a33f08d0ecd4
thomas-rooty <essainono1@gmail.com>
2022-04-19 20:42:54 +0200

    created header component model

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="App">
+            <Header/>
         </div>
     );
 };
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="upper-container">
+                <h1>Quentin Maker</h1>
+            </div>
+            <div className="lower-container">
+                <h2>Vidéaste - Photographe - Réalisateur</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
