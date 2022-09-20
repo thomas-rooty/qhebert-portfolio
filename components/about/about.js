fa13aadd55649d1e34e4d5103bdd8f6beb578c06
Thomas Caron <tc.caron91@gmail.com>
2022-09-20 14:12:47 +0200

    indented text

--- components/about/about.js
+++ components/about/about.js
@@ -15,7 +15,10 @@ const About = () => {
 			<div className={styles.right}>
 				<h1>A PROPOS</h1>
 				<p>Tout a commencé par la publication de tutoriels dans le domaine du bricolage sur YouTube.</p>
-				<p>Progressivement, je me suis spécialisé dans la réalisation de vidéos dynamiques. Chaque vidéo est soigneusement réfléchie et travaillée de A à Z par mes soins, en commencant par le storyboard, puis le tournage, le montage, la colorimétrie et le sound design.</p>
+				<p>Progressivement, je me suis spécialisé dans la réalisation de vidéos dynamiques.
+					Chaque vidéo est soigneusement réfléchie et travaillée de A à Z par mes soins, en commencant par le
+					storyboard,
+					puis le tournage, le montage, la colorimétrie et le sound design.</p>
 				<p>J'utilise régulièrement des drones pour obtenir des images aériennes.</p>
 				<p>Ma passion pour l'image me permet aujourd'hui de travailler avec des sportifs et artistes de haut niveau.</p>
 			</div>
