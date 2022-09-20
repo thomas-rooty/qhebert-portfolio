485af9d4a508bba734ea4bd3205a2502b71fa34e
Thomas Caron <tc.caron91@gmail.com>
2022-09-20 10:26:13 +0200

    fixed img not showing

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,15 +1,12 @@
 import styles from '../../styles/Footer.module.css'
 import Image from 'next/image'
-import Instagram from '/assets/icons/instagram.svg'
-import Facebook from '/assets/icons/facebook.svg'
-import Youtube from '/assets/icons/youtube.svg'
 
 const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.footer__icon}>
 				<Image
-					src={Instagram}
+					src={'/assets/icons/instagram.svg'}
 					alt="Instagram"
 					width={30}
 					height={30}
@@ -17,7 +14,7 @@ const Footer = () => {
 			</div>
 			<div className={styles.footer__icon}>
 				<Image
-					src={Facebook}
+					src={'/assets/icons/facebook.svg'}
 					alt="Facebook"
 					width={30}
 					height={30}
@@ -25,7 +22,7 @@ const Footer = () => {
 			</div>
 			<div className={styles.footer__icon}>
 				<Image
-					src={Youtube}
+					src={'/assets/icons/youtube.svg'}
 					alt="Youtube"
 					width={30}
 					height={30}
