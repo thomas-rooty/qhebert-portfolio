863f8351a16c55f40d733d44828a42b9ae4a2e46
Thomas Caron <essainono1@gmail.com>
2022-09-23 14:06:19 +0200

    deleted unused files + added 2 portraits (#8)
    * changed video
    
    * moved text + fixed about section proportion
    
    * added photos page
    
    * compressed images
    
    * compressed video
    
    * deleted unused files + added 2 portraits

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -5,7 +5,7 @@ const Gallery = () => {
 		<div className={styles.gallery}>
 			<h1>PHOTOS</h1>
 			<div className={styles.portraitContainer}>
-				{[...Array(13)].map((_, i) => (
+				{[...Array(15)].map((_, i) => (
 					<div className={styles.portrait} key={i}>
 						<img
 							src={`/assets/pictures_gallery/portrait/${i + 1}.jpg`}
@@ -15,7 +15,7 @@ const Gallery = () => {
 				))}
 			</div>
 			<div className={styles.landscapeContainer}>
-				{[...Array(4)].map((_, i) => (
+				{[...Array(2)].map((_, i) => (
 					<div className={styles.landscape} key={i}>
 						<img
 							src={`/assets/pictures_gallery/landscape/${i + 1}.jpg`}
