74e1d8dabed0fb4a34eeb559196813c53f4af34a
Thomas Caron <tc.caron91@gmail.com>
2022-09-27 15:27:21 +0200

    autoplay on mobile

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -3,7 +3,7 @@ import styles from '../../styles/Hero.module.css'
 const Hero = () => {
 	return (
 		<div className={styles.hero}>
-			<video className={styles.heroVideo} autoPlay loop muted>
+			<video className={styles.heroVideo} playsInline autoPlay loop muted>
 				<source src="/assets/videos/hero.mp4" type="video/mp4"/>
 			</video>
 			<div className={styles.centerBottom}>
