0460d9da99f34d7d92e2e7ea1ad88c5a262f8d9e
Thomas Caron <tc.caron91@gmail.com>
2022-09-27 11:59:02 +0200

    added image modal onClick

--- components/photosLib/photosLib.js
+++ components/photosLib/photosLib.js
@@ -1,5 +1,5 @@
 import styles from "../../styles/Photos.module.css";
-import Image from "next/future/image";
+import ModalImage from "react-modal-image";
 import portrait1 from "../../public/assets/pictures_gallery/portrait/1.jpg";
 import portrait2 from "../../public/assets/pictures_gallery/portrait/2.jpg";
 import portrait3 from "../../public/assets/pictures_gallery/portrait/3.jpg";
@@ -19,63 +19,149 @@ import landscape1 from "../../public/assets/pictures_gallery/landscape/1.jpg";
 import landscape2 from "../../public/assets/pictures_gallery/landscape/2.jpg";
 
 const PhotosLib = () => {
+
 	return (
 		<div className={styles.gallery}>
 			<h1>PHOTOS</h1>
 			<div className={styles.portraitContainer}>
 				<div className={styles.portrait} key={portrait1}>
-					<Image src={portrait1} alt="portrait1"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/1.jpg"}
+						medium={"/assets/pictures_gallery/portrait/1.jpg"}
+						large={"/assets/pictures_gallery/portrait/1.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait2}>
-					<Image src={portrait2} alt="portrait2"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/2.jpg"}
+						medium={"/assets/pictures_gallery/portrait/2.jpg"}
+						large={"/assets/pictures_gallery/portrait/2.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait3}>
-					<Image src={portrait3} alt="portrait3"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/3.jpg"}
+						medium={"/assets/pictures_gallery/portrait/3.jpg"}
+						large={"/assets/pictures_gallery/portrait/3.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait4}>
-					<Image src={portrait4} alt="portrait4"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/4.jpg"}
+						medium={"/assets/pictures_gallery/portrait/4.jpg"}
+						large={"/assets/pictures_gallery/portrait/4.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait5}>
-					<Image src={portrait5} alt="portrait5"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/5.jpg"}
+						medium={"/assets/pictures_gallery/portrait/5.jpg"}
+						large={"/assets/pictures_gallery/portrait/5.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait6}>
-					<Image src={portrait6} alt="portrait6"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/6.jpg"}
+						medium={"/assets/pictures_gallery/portrait/6.jpg"}
+						large={"/assets/pictures_gallery/portrait/6.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait7}>
-					<Image src={portrait7} alt="portrait7"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/7.jpg"}
+						medium={"/assets/pictures_gallery/portrait/7.jpg"}
+						large={"/assets/pictures_gallery/portrait/7.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait8}>
-					<Image src={portrait8} alt="portrait8"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/8.jpg"}
+						medium={"/assets/pictures_gallery/portrait/8.jpg"}
+						large={"/assets/pictures_gallery/portrait/8.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait9}>
-					<Image src={portrait9} alt="portrait9"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/9.jpg"}
+						medium={"/assets/pictures_gallery/portrait/9.jpg"}
+						large={"/assets/pictures_gallery/portrait/9.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait10}>
-					<Image src={portrait10} alt="portrait10"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/10.jpg"}
+						medium={"/assets/pictures_gallery/portrait/10.jpg"}
+						large={"/assets/pictures_gallery/portrait/10.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait11}>
-					<Image src={portrait11} alt="portrait11"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/11.jpg"}
+						medium={"/assets/pictures_gallery/portrait/11.jpg"}
+						large={"/assets/pictures_gallery/portrait/11.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait12}>
-					<Image src={portrait12} alt="portrait12"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/12.jpg"}
+						medium={"/assets/pictures_gallery/portrait/12.jpg"}
+						large={"/assets/pictures_gallery/portrait/12.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait13}>
-					<Image src={portrait13} alt="portrait13"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/13.jpg"}
+						medium={"/assets/pictures_gallery/portrait/13.jpg"}
+						large={"/assets/pictures_gallery/portrait/13.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait14}>
-					<Image src={portrait14} alt="portrait14"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/14.jpg"}
+						medium={"/assets/pictures_gallery/portrait/14.jpg"}
+						large={"/assets/pictures_gallery/portrait/14.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 				<div className={styles.portrait} key={portrait15}>
-					<Image src={portrait15} alt="portrait15"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/portrait/15.jpg"}
+						medium={"/assets/pictures_gallery/portrait/15.jpg"}
+						large={"/assets/pictures_gallery/portrait/15.jpg"}
+						className={styles.imgPortrait}
+					/>
 				</div>
 
 			</div>
 			<div className={styles.landscapeContainer}>
 				<div className={styles.landscape} key={landscape1}>
-					<Image src={landscape1} alt="landscape1"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/landscape/1.jpg"}
+						medium={"/assets/pictures_gallery/landscape/1.jpg"}
+						large={"/assets/pictures_gallery/landscape/1.jpg"}
+						className={styles.imgLandscape}
+					/>
 				</div>
 				<div className={styles.landscape} key={landscape2}>
-					<Image src={landscape2} alt="landscape2"/>
+					<ModalImage
+						small={"/assets/pictures_gallery/landscape/2.jpg"}
+						medium={"/assets/pictures_gallery/landscape/2.jpg"}
+						large={"/assets/pictures_gallery/landscape/2.jpg"}
+						className={styles.imgLandscape}
+					/>
 				</div>
 			</div>
 		</div>
