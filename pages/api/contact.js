a8e064f1a5598af5222ddbc1f862770d043dd765
thomas-rooty <essainono1@gmail.com>
2022-11-02 13:58:39 +0100

    added promise

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,16 +1,33 @@
-export default function (req, res) {
-  require('dotenv').config()
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-  let nodemailer = require('nodemailer')
+export default async (req, res) => {
+
+  const {name, email, message} = JSON.parse(JSON.stringify(req.body));
+
   const transporter = nodemailer.createTransport({
-    port: process.env.NEXT_PUBLIC_PORT,
-    host: process.env.NEXT_PUBLIC_H0ST,
+    port: 465,
+    host: process.env.NEXT_PUBLIC_HOST,
     auth: {
       user: process.env.NEXT_PUBLIC_EMAIL,
       pass: process.env.NEXT_PUBLIC_PASSWORD,
     },
     secure: true,
-  })
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
   const mailData = {
     from: process.env.email,
     to: 'tc.caron91@gmail.com',
@@ -18,12 +35,21 @@ export default function (req, res) {
     text: req.body.message + " | Envoyé de: " + req.body.email,
     html: `<div>${req.body.message}</div><p>Envoyé de:
     ${req.body.email}</p>`
-  }
-  transporter.sendMail(mailData, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info)
-  })
-  res.send('success')
-}
+  };
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.error(err);
+        console.log(mailData);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+
+  res.status(200).json({status: "OK"});
+};
